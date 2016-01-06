9105e8370530fa1b617f9c21d71edb2abc8bc813
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2016-01-06 00:31:23 -0500

    added misra

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource rahul');
+  res.send('respond with a resource rahul misra');
 });
 
 module.exports = router;
